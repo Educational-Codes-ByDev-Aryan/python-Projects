9d2922eb9185cf11d1765f0c953dd1abaec19367
A-885 <gosaviaryan76@gmail.com>
2026-07-02 22:29:27 +0530

    add passStrenChecker

---
+++ py_project_4/passStrenChecker.py
@@ -0,0 +1,28 @@
+
+while True:
+    password = input("Enter a password\n")
+    length = password.__len__()
+    digit = 0
+    count = 0
+    print("The length of the password is", length)
+
+# conditions applyed
+    if length < 8:
+        print("The password must be at least 8 characters")
+
+    else:
+        for n in password:
+            if n.isdigit():
+                digit += 1
+        if digit <= 2:
+            print("should contain at least 2 digits")
+        else:
+            for ch in password:
+                if ch.isupper():
+                    count += 1
+        if count <= 3:
+            print("should contain atleast 3 upper case letters")
+
+        else:
+            print("Your password is strongest password")
+            exit()
